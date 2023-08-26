dc0d331968309c83aba436138f5157611e493859
KamleshTechuz <kamlesh@techuz.com>
2023-08-26 21:24:37 +0530

    white space in label

--- src/components/StyledInputLabel.tsx
+++ src/components/StyledInputLabel.tsx
@@ -6,7 +6,7 @@ interface styledInputLabel {
 const StyledInputLabel = ({ label, isRequired }: styledInputLabel) => {
   return (
     <>
-      <label htmlFor="input" className="block font-bold text-gray-700 px-1">
+      <label htmlFor="input" className="block font-bold text-gray-700 px-1 whitespace-nowrap">
         {label}
         {isRequired && <span className="text-[#0dceda] ml-1">*</span>}
       </label>
